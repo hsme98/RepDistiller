435e0a5112d3e14a201276029e74822327a440bc
Hasan Sabri Melihcan Erol <melihcanerol98@gmail.com>
2024-02-27 20:01:55 -0500

    modified train_teachers.sh

--- scripts/run_train_teachers.sh
+++ scripts/run_train_teachers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH -o train_teacher.log-%j-%a
-#SBATCH -a 1-8
-#SBATCH -c 4
+#SBATCH -a 1
+#SBATCH -c 12
 #SBATCH --gres=gpu:volta:1
 
 source /etc/profile
-module load anaconda/2020a
-source activate distill_env
-
-
+module load anaconda/2022a
+module load cuda/11.8
+source activate distil_env5
+echo begin
 python /home/gridsan/herol/RepDistiller/train_teacher.py --model resnet20 --trial $SLURM_ARRAY_TASK_ID
